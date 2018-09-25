af5c45c7be22472757a5600f408c20af60a0c9df
Sidharth D <sidharth@firstbytedigital.com>
2018-09-25 14:54:31 +0530

    Tidied up App.js, store.js, NoteScreen.js

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { createStore } from 'redux';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
@@ -8,11 +6,10 @@ import AppRouter from '@routers/AppRouter.js';
 import rootReducer from '@reducers';
 import { store, persistor } from '@config';
 
-
 export default class App extends Component{
   render() {
     return (
-      <StoreProvider store={store} style={styles.container}>
+      <StoreProvider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <AppRouter />
         </PersistGate>
@@ -20,13 +17,3 @@ export default class App extends Component{
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-});
--- src/config/store.js
+++ src/config/store.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-//import { purgeStoredState } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducers from '../reducers';
 
 const persistConfig = { key: 'store', storage }
-//purgeStoredState(persistConfig)
 const persistedReducer = persistReducer(persistConfig, reducers)
 const store = createStore(persistedReducer,applyMiddleware(ReduxThunk));
-let persistor = persistStore(store)
+const persistor = persistStore(store)
 
 export { store, persistor };
