687921ebcc18de4ac98aee946fcbd52575edf6a7
Sidharth D <sidharth@firstbytedigital.com>
2018-09-27 15:13:44 +0530

    Add a note only if either title or content is present

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -11,14 +11,19 @@ const initialState = { notes: [], currentNote: {} }
 
 export default (state = initialState, action) => {
   console.log(action);
+  
   const { type, payload } = action;
 
   switch(type) {
     case ADD_NOTE: {
       const { title, content } = payload;
-      const key = shortid.generate();
-      const notes = [...state.notes, { title, content, key }]
-      return {...state, notes};
+      if (title || content) {
+        // Add a note only if either title or content is present.
+        const key = shortid.generate();
+        const notes = [...state.notes, { title, content, key }]
+        return {...state, notes};
+      }
+      return { ...state }
     }
 
     case EDIT_NOTE: {
