f696a3309680984268dd239eca11bf8740222d24
Sidharth D <sidharth@firstbytedigital.com>
2018-09-28 18:43:44 +0530

    Added icons and splash screens for Android

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import { Provider as StoreProvider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen'
 
 import AppRouter from '@routers/AppRouter.js';
 import { store, persistor } from '@config/store';
 
 export default class App extends Component{
+  componentDidMount() {
+    SplashScreen.hide();
+  }
   render() {
     return (
       <StoreProvider store={store}>
